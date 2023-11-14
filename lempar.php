f183079b22a5eb15d24de33c6f667f72f7d2f0c5
Raven6th <145556185+Raven6th@users.noreply.github.com>
2023-11-14 20:51:12 +0800

    Update lempar.php
    cegah admin untuk menambah keranjang

--- lempar.php
+++ lempar.php
@@ -10,10 +10,16 @@
             $_SESSION['keranjang'][$id_produk]+=1;
         }
         else {
+            // ['keranjang'] merupakan array, sehingga setiap 
+            // $id_produk akan disimpan dengan nilai yang berbeda
             $_SESSION['keranjang'][$id_produk] = 1;
             echo "<script>alert('Barang di tambahkan ke dalam keranjang')</script>";
             echo "<script>location='index.php#menu'</script>";
         }
+    }
+    else if ($_SESSION["akses"] === "admin") {
+        echo "<script>alert('Admin tidak dapat menambah item ke keranjang')</script>";
+        echo "<script>location='index.php#menu'</script>";
     }
     else {
         echo "<script>alert('Anda belum login! mohon login terlebih dahulu sebelum belanja')</script>";
